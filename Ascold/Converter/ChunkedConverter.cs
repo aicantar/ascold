78c55f35df0a63ae7ed9212b4a1492fcdc1322f8
Phil Chekhov <me@aicantar.com>
2024-01-07 17:43:02 +0600

    Fix ASCII string separation issue for some image sizes

--- Ascold/Converter/ChunkedConverter.cs
+++ Ascold/Converter/ChunkedConverter.cs
@@ -7,15 +7,18 @@ public class ChunkedConverter(IConverter baseConverter) : IConverter
 {
     private const int ChunkWidth = 5, ChunkHeight = 12, ImageHeight = ChunkHeight * 40;
 
-    public string BitmapToAscii(SKBitmap bitmap) =>
-        string.Join(
+    public string BitmapToAscii(SKBitmap bitmap)
+    {
+        var resizedBitmap = ResizeIfNeeded(bitmap);
+
+        return string.Join(
             "",
-            ResizeIfNeeded(bitmap)
+            resizedBitmap
                 .Chunk(ChunkWidth, ChunkHeight)
                 .Select((chunk, idx) =>
                 {
                     var ascii = baseConverter.BitmapToAscii(chunk);
-                    var nChunksW = bitmap.Width / ChunkWidth;
+                    var nChunksW = resizedBitmap.Width / ChunkWidth;
 
                     return string.Format(
                         "{0}{1}",
@@ -26,6 +29,7 @@ public class ChunkedConverter(IConverter baseConverter) : IConverter
                     );
                 })
         );
+    }
 
     private SKBitmap ResizeIfNeeded(SKBitmap bitmap) =>
         bitmap.Height > ImageHeight
